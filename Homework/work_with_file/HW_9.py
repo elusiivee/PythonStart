71f87cee11b733bb28f1535be51e12630bd612c9
elusiivee <andrii.1669@gmail.com>
2023-10-08 15:45:33 +0200

    adde work with file

--- Homework/work_with_file/HW_9.py
+++ Homework/work_with_file/HW_9.py
@@ -4,24 +4,61 @@ with open('./hw_10_test.txt','r', encoding='UTF-8') as f:
         value_list.append(i.split())
 
 category_dict={}
+names={}
+speciic_name={}
 def category(list:list):
     for item  in list:
-        category = item[-1]
-        value = round(float(item[-2].replace('$', '')),2)
-        if category not in category_dict:
-            category_dict[category] = value
+        categorys = item[-1]
+        value = float(item[-2].replace('$', ''))
+        if categorys not in category_dict:
+            category_dict[categorys] = value   #чому воно не округляється
         else:
-            category_dict[category] += value
+            category_dict[categorys] += value
     return category_dict
 
+def expenses_each_name():
+    with open('hw_10_test.txt', 'r', encoding='UTF-8') as file:
+        for item in file:
+            name_part= item.split()
+            name = "".join(name_part[1])
+            value_str = name_part[-2]  # Assuming the value is the third-to-last element
+            value = float(value_str.replace('$', ''))
+            if name not in names:
+                names[name]=value
+            else:
+                names[name]+=value
+    return names
 
-print(value_list)
+def get_spec_name(spec_name:str):
+    with open('hw_10_test.txt', 'r', encoding='UTF-8') as file:
+        for item in file:
+            name_part= item.split()
+            name = "".join(name_part[1])
+            value_str = name_part[-2]  # Assuming the value is the third-to-last element
+            value = float(value_str.replace('$', ''))
+            if spec_name not in names:
+                return 'No person'
+            else:
+                return f'Count of purchase: {file.read().count(spec_name)}. Total price: {round(names.get(spec_name),2)}'
+    return names
+
 
 
 category(value_list)
+expenses_each_name()
+print('Category_____________')
 
-for category, total in category_dict.items():
-    print(f'{category}: ${total}')
+for category, amount in category_dict.items():
+    print(f'{category}: ${round(amount,2)}')
+
+print('Name_____________')
+for name, amount in names.items():
+    print(f'{name}: ${round(amount,2)}')
+
+spec_name=input('Input name(Bob/Mary/Aleksa/Maria/Jack): ')
+
+print(get_spec_name(spec_name))
+
 
 
 
